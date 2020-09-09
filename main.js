04996ffe0538901d9cd5333f30a2853e6dcf418c
Trifle22 <xromchenkov99@gmail.com>
2020-09-09 20:20:55 +0300

    +moveroad + traffic

--- main.js
+++ main.js
@@ -13,16 +13,41 @@ const keys = {
   ArrowUp : false,
   ArrowDown : false,
   ArrowRight : false,
-  ArrowLeft : false
+  ArrowLeft : false,
+  F5 : false,
+  F12: false,
 }
 const setting = {
   start: false,
   score: 0,
   speed : 3,
+  traffic : 3
+}
+
+function getQuantityELements(heightElement) {
+   return document.documentElement.clientHeight / heightElement + 1;
 }
 
 function startGame() {
   start.classList.add('hide');
+
+  for (let i = 0; i < getQuantityELements(100); i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    line.style.top = (i * 100) + 'px';
+    line.y = i*100;
+    gameArea.appendChild(line);
+  }
+  for (let i = 0; i < getQuantityELements(100 * setting.traffic); i++ ) {
+    const enemy = document.createElement('div');
+    enemy.classList.add('enemy');
+    enemy.y = -100 * setting.traffic * (i + 1);
+    enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+    enemy.style.top = enemy.y + 'px';
+    enemy.style.background = 'transparent url(./image/enemy2.png) center / cover no-repeat';
+    gameArea.appendChild(enemy);
+  }
+
   setting.start = true;
   gameArea.appendChild(car);
   setting.x = car.offsetLeft;
@@ -31,8 +56,9 @@ function startGame() {
 }
 
 function playGame() {
-  console.log('play game');
   if (setting.start) {
+    moveRoad();
+    moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
       setting.x -= setting.speed;
     }
@@ -59,4 +85,29 @@ function startRun(event) {
 function stopRun(event) {
   event.preventDefault();
   keys[event.key] = false;
+}
+
+function moveRoad() {
+  let lines = document.querySelectorAll('.line');
+  lines.forEach(function(line) {
+    line.y += setting.speed;
+    line.style.top = line.y + 'px';
+    if (line.y >= document.documentElement.clientHeight) {
+      line.y = -100;
+    }
+    })
+}
+
+function moveEnemy() {
+  let enemy = document.querySelectorAll('.enemy');
+
+  enemy.forEach(function(item){
+    item.y += setting.speed / 2;
+    item.style.top = item.y + 'px';
+     if (item.y >= document.documentElement.clientHeight) {
+    item.y = -100 * setting.traffic;
+    item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px'; 
+  }
+  });
+
 }
