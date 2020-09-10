520ffc780cf04faab45e34f53f39189e56b05b69
Trifle22 <xromchenkov99@gmail.com>
2020-09-10 13:59:56 +0300

    +clashes

--- main.js
+++ main.js
@@ -30,7 +30,8 @@ function getQuantityELements(heightElement) {
 
 function startGame() {
   start.classList.add('hide');
-
+  gameArea.innerHTML = '';
+  
   for (let i = 0; i < getQuantityELements(100); i++) {
     const line = document.createElement('div');
     line.classList.add('line');
@@ -47,9 +48,12 @@ function startGame() {
     enemy.style.background = 'transparent url(./image/enemy2.png) center / cover no-repeat';
     gameArea.appendChild(enemy);
   }
-
+  setting.score = 0;
   setting.start = true;
   gameArea.appendChild(car);
+  car.style.left = (gameArea.offsetWidth / 2) - (car.offsetWidth / 2);
+  car.style.top = 'auto';
+  car.style.bottom = '10px';
   setting.x = car.offsetLeft;
   setting.y = car.offsetTop;
   requestAnimationFrame(playGame);
@@ -57,6 +61,8 @@ function startGame() {
 
 function playGame() {
   if (setting.start) {
+    setting.score += setting.speed;
+    score.innerHTML = 'Score <br>' + setting.score;
     moveRoad();
     moveEnemy();
     if (keys.ArrowLeft && setting.x > 0) {
@@ -102,6 +108,20 @@ function moveEnemy() {
   let enemy = document.querySelectorAll('.enemy');
 
   enemy.forEach(function(item){
+    let carRect = car.getBoundingClientRect();
+    let enemyRect = item.getBoundingClientRect();
+
+    if (carRect.top <= enemyRect.bottom &&
+        carRect.right >= enemyRect.left &&
+        carRect.left <= enemyRect.right &&
+        carRect.bottom >= enemyRect.top)  {
+          setting.start = false;
+          console.warn('DTP');
+          start.classList.remove('hide');
+          start.style.top = score.offsetHeight;
+          score.style.top = 0 + 'px';
+      }
+
     item.y += setting.speed / 2;
     item.style.top = item.y + 'px';
      if (item.y >= document.documentElement.clientHeight) {
